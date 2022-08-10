9da780826285c4a0eec17304e669fb7d6352c529
ricky <ricky@psp-power.com.tw>
2022-08-10 11:34:00 +0800

    Dynamic Routes - 使用 next/router 取得特定網頁

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,5 +1,11 @@
+// If you want to access the `router` object inside any function component in your app, use the useRouter hook
+import { useRouter } from 'next/router' 
+
 function ProductDetail() {
-  return <h1>Product Detail</h1>
+  const router = useRouter()
+  // `query` The query string parsed to an object, including dynamic route parameters. It will be an empty object during prerendering if the page doesn't use Server-side Rendering. Defaults to `{}`
+  const productID = router.query.id
+  return <h1>Product Detail {productID}</h1>
 }
 
 export default ProductDetail
