6d68850758b046aec4f22648fc641e54170d9bcc
ricky <ricky@jvdiamondtech.com>
2022-08-20 23:46:13 +0800

    Navigating Programmatically - use button on click

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,6 +1,12 @@
 import Link from "next/link"
+import Router, { useRouter } from 'next/router'
 
 function Product( { productId = 100 } ) {
+  const router = useRouter()
+  const handleClick = ()=>{
+    console.log('A')
+    router.push('/blog')
+  }
   return (
     <>
       <Link href="/">
@@ -18,6 +24,7 @@ function Product( { productId = 100 } ) {
           Product {productId}
         </a>
       </Link>
+      <button onClick={handleClick}>Go Blog</button>
     </>
   )
 }
